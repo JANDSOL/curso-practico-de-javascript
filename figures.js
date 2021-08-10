8f6c144bea09cb4d7e38b71f57afecbdf0f13cdd
Juan Andrade <jpas.juan23@gmail.com>
2021-08-10 11:00:06 -0500

    Convert static variables to triangle functions.

--- figures.js
+++ figures.js
@@ -11,26 +11,30 @@ function squarePerimeter(side) {
 
 function squareArea(side) {
     return side ** 2;
-} 
+}
 // squareArea()
 // console.log('El área del cuadrado es: ' + squareArea + 'cm^2');
 console.groupEnd();
 
 // Triangle code
 console.group('Triángulos');
-const triangleSide1 = 6;
-const triangleSide2 = 6;
-const triangleBase = 4;
-const triangleHeight = 5.5;
-const trianglePerimeter = triangleSide1 + triangleSide2 + triangleBase;
-const triangleArea = (triangleBase * triangleHeight) / 2;
-console.log(
-    'Los lados del triángulo miden: '
-    + triangleSide1 + 'cm, '
-    + triangleSide2 + 'cm, '
-    + triangleBase + 'cm'
-);
-console.log('La altura del triángulo es: ' + triangleHeight + 'cm');
-console.log('El perímetro del triángulo es: ' + trianglePerimeter + 'cm');
-console.log('El área del triángulo es: ' + triangleArea + 'cm^2');
+// const triangleSide1 = 6;
+// const triangleSide2 = 6;
+// const triangleBase = 4;
+// const triangleHeight = 5.5;
+function trianglePerimeter(side1, side2, base) {
+    return side1 + side2 + base;
+}
+function triangleArea(base, height) {
+    return (base * height) / 2;
+}
+// console.log(
+//     'Los lados del triángulo miden: '
+//     + triangleSide1 + 'cm, '
+//     + triangleSide2 + 'cm, '
+//     + triangleBase + 'cm'
+// );
+// console.log('La altura del triángulo es: ' + triangleHeight + 'cm');
+// console.log('El perímetro del triángulo es: ' + trianglePerimeter + 'cm');
+// console.log('El área del triángulo es: ' + triangleArea + 'cm^2');
 console.groupEnd();
