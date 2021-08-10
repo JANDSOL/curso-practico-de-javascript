c0d2b3c2026597060b45d6d194c805f68f19b7c3
Juan Andrade <jpas.juan23@gmail.com>
2021-08-09 21:53:12 -0500

    Declaration and initialization of variables for square.

---
+++ figures.js
@@ -0,0 +1,11 @@
+// Square code
+console.group('Cuadrados');
+const squareSide = 5;
+console.log('Los lados del cuadrado miden: ' + squareSide + 'cm');
+
+const squarePerimeter = squareSide * 4;
+console.log('El perímetro del cuadrado es: ' + squarePerimeter + 'cm');
+
+const squareArea = squareSide ** 2;
+console.log('El área del cuadrado es: ' + squareArea + 'cm^2');
+console.groupEnd();
