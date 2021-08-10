6d239c5e8bcbb5c1a82af6627e5facff85a3f1bb
Juan Andrade <jpas.juan23@gmail.com>
2021-08-09 21:42:02 -0500

    Creation of files and first html structure.

---
+++ figures.js
@@ -0,0 +1,9 @@
+// Square code
+const ladoCuadrado = 5;
+console.log('Los lados del cuadrado miden: ' + ladoCuadrado + 'cm');
+
+const preimetroCuadrado = ladoCuadrado * 4;
+console.log('El perímetro del cuadrado es: ' + preimetroCuadrado + 'cm');
+
+const areaCuadrado = ladoCuadrado ** 2;
+console.log('El área del cuadrado es: ' + areaCuadrado + 'cm2');
