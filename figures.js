86a004303a24cea73adf27e6087f9edf534e1b95
Juan Andrade <jpas.juan23@gmail.com>
2021-08-09 22:13:59 -0500

    Declaration and initialization of variables for triangle.

--- figures.js
+++ figures.js
@@ -9,3 +9,22 @@ console.log('El perímetro del cuadrado es: ' + squarePerimeter + 'cm');
 const squareArea = squareSide ** 2;
 console.log('El área del cuadrado es: ' + squareArea + 'cm^2');
 console.groupEnd();
+
+// Triangle code
+console.group('Triángulos');
+const triangleSide1 = 6;
+const triangleSide2 = 6;
+const triangleBase = 4;
+const triangleHeight = 5.5;
+const trianglePerimeter = triangleSide1 + triangleSide2 + triangleBase;
+const triangleArea = (triangleBase * triangleHeight) / 2;
+console.log(
+    'Los lados del triángulo miden: '
+    + triangleSide1 + 'cm, '
+    + triangleSide2 + 'cm, '
+    + triangleBase + 'cm'
+);
+console.log('La altura del triángulo es: ' + triangleHeight + 'cm');
+console.log('El perímetro del triángulo es: ' + trianglePerimeter + 'cm');
+console.log('El área del triángulo es: ' + triangleArea + 'cm^2');
+console.groupEnd();
