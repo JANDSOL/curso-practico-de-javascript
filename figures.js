29eac86fefd2b722f74e20dc6f965295c3c21e69
Juan Andrade <jpas.juan23@gmail.com>
2021-08-11 10:51:18 -0500

    Html+JavaScript operating mechanism for calculating the triangle.

--- figures.js
+++ figures.js
@@ -53,22 +53,68 @@ console.groupEnd();
 // Here I interact whit the HTML.
 function calculateSquarePerimeter() {
     const input = document.getElementById('SquareInput');
-    const value = input.value;
-    if (value > 0) {
-        const perimeter = squarePerimeter(value);
-        alert(perimeter.toFixed(2));
+    const value = parseFloat(input.value);
+    if (!Number.isNaN(value)) {
+        if (value > 0) {
+            const perimeter = squarePerimeter(value);
+            alert(perimeter.toFixed(2));
+        } else {
+            alert('Ingrese un número mayor a 0.');
+        }
     } else {
-        alert('Ingrese un número mayor a 0.');
+        alert('Ingresa un valor para realizar el cálculo.');
     }
 };
 
 function calculateSquareArea() {
     const input = document.getElementById('SquareInput');
-    const value = input.value;
-    if (value > 0) {
-        const area = squareArea(value);
-        alert(area.toFixed(2));
+    const value = parseFloat(input.value);
+    if (!Number.isNaN(value)) {
+        if (value > 0) {
+            var area = squareArea(value);
+            alert(area.toFixed(2));
+        } else {
+            alert('Ingrese un número mayor a 0.');
+        }
     } else {
-        alert('Ingrese un número mayor a 0.');
+        alert('Ingresa un valor para realizar el cálculo.');
+    }
+};
+
+function calculateTrianglePerimeterArea(area) {
+    var validationOf0 = [];
+    var validationOfNan = [];
+    const inputSide1 = document.getElementById('TriangleInput1');
+    const inputSide2 = document.getElementById('TriangleInput2');
+    const inputBase = document.getElementById('TriangleInputBase');
+    const valueSide1 = parseFloat(inputSide1.value);
+    const valueSide2 = parseFloat(inputSide2.value);
+    const valueBase = parseFloat(inputBase.value);
+    // Input validation.
+    if (!Number.isNaN(valueSide1) && !Number.isNaN(valueSide2) && !Number.isNaN(valueBase)) {
+        if (valueSide1 > 0 && valueSide2 > 0 && valueBase > 0) {
+            if (!area) {  // Calculate perimeter
+                var perimeter = trianglePerimeter(valueSide1, valueSide2, valueBase);
+                alert(perimeter.toFixed(2));
+            } else {  // Calculate area
+                const height = (valueSide1 + valueSide2) / valueBase;
+                var area = triangleArea(valueBase, height);
+                alert(area.toFixed(2));
+            }
+        } else {
+            if (valueSide1 <= 0) { validationOf0.push('El primer lado del triángulo necesita un número mayor a 0.'); }
+            if (valueSide2 <= 0) { validationOf0.push('El segundo lado del triángulo necesita un número mayor a 0.'); }
+            if (valueBase <= 0) { validationOf0.push('La base del triángulo necesita un número mayor a 0.'); }
+            validationOf0.forEach(function (value) {
+                alert(value);
+            });
+        }
+    } else {
+        if (Number.isNaN(valueSide1)) { validationOfNan.push('El primer lado del triángulo necesita un valor.'); }
+        if (Number.isNaN(valueSide2)) { validationOfNan.push('El segundo lado del triángulo necesita un valor.'); }
+        if (Number.isNaN(valueBase)) { validationOfNan.push('La base del triángulo necesita un valor.'); }
+        validationOfNan.forEach(function (value) {
+            alert(value);
+        });
     }
 };
